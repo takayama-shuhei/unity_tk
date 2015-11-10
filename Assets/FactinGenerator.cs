3452209a555675bacca3240a861b2561b861c0be
高山周平 <shuhei@takayamashuuhei-no-MacBook-Pro.local>
2015-11-11 02:36:34 +0900

    thanks....

--- Assets/FactinGenerator.cs
+++ Assets/FactinGenerator.cs
@@ -6,12 +6,15 @@ using System.Collections;
 public class FactinGenerator : MonoBehaviour
 {
 
-    // すべてのMicroTubeに共通の設定になると思うので，
-    // colorとwidthは生成するクラスが持ってる
-    // この値はインスペクタからいじれる(SerializeField)
-    [SerializeField] Color color;
-    [SerializeField] float width;
-
+	// すべてのMicroTubeに共通の設定になると思うので，
+	// colorとwidthは生成するクラスが持ってる
+	// この値はインスペクタからいじれる(SerializeField)
+	[SerializeField]
+	Color
+		color;
+	[SerializeField]
+	float
+		width;
 	float r1 = 621.09f, r2 = 690.10f, r, theta, phi, layer1, layer2;
 	List<MicroTube> actin = new List<MicroTube> ();
 
@@ -19,104 +22,267 @@ public class FactinGenerator : MonoBehaviour
 	{
 		layer1 = (r2 - r1) / 3f + r1;
 		layer2 = 2f * (r2 - r1) / 3f + r1;
-		//for (j=0; j<19; j++) {
-		var j = 1;
-		for (var i = 0; i < 36; i++) {
-			r = r1;
-			phi = i / 18f * 3.14f;
-			theta = j / 18f * 3.14f;
-			float x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
-			float y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
-			float z = r * Mathf.Cos (theta);
-			var start = new Vector3(x, y, z);
-
-			r = layer1;
-			phi = i / 18f * 3.14f;
-			theta = j / 18f * 3.14f;
-			x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
-			y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
-			z = r * Mathf.Cos (theta);
-			var end = new Vector3(x, y, z);
-
-			var microTube = MicroTube.Instantiate(start, end);
-            microTube.ApplyColor(color);
-            microTube.ApplyWidth(width);
-            microTube.transform.SetParent(transform, false);
-            actin.Add(microTube);
-
-			/*
-				phi = i  / 18f * 3.14f;
-				theta = j+1  / 18f * 3.14f;
+		for (var j=0; j<19; j++) {
+			//var j = 1 ;
+			for (var i = 0; i < 36; i++) {
+				r = r1;
+				phi = i / 18f * 3.14f;
+				theta = j / 18f * 3.14f;
+				float x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				float y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				float z = r * Mathf.Cos (theta);
+				var start = new Vector3 (x, y, z);
+				
+				r = layer1;
+				phi = i / 18f * 3.14f;
+				theta = j / 18f * 3.14f; 
 				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
 				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
 				z = r * Mathf.Cos (theta);
-				end.setPoint (x, y, z);
-				actin.Add (start);
-				actin.Add (end);
-
-				phi = i  / 18f * 3.14f;
-				theta = j-1  / 18f * 3.14f;
+				var end = new Vector3 (x, y, z);
+				var microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 + 10) / 180f * 3.14f;
+				theta = j / 18f * 3.14f;
 				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
 				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
 				z = r * Mathf.Cos (theta);
-				end.setPoint (x, y, z);
-				actin.Add (start);
-				actin.Add (end);
-
-				phi = i-1  / 18f * 3.14f;
-				theta = j  / 18f * 3.14f;
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 - 10) / 180f * 3.14f;
+				theta = j / 18f * 3.14f;
 				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
 				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
 				z = r * Mathf.Cos (theta);
-				end.setPoint (x, y, z);
-				actin.Add (start);
-				actin.Add (end);;
-
-				phi = i-1  / 18f * 3.14f;
-				theta = j+1  / 18f * 3.14f;
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				
+				
+				
+				phi = i / 18f * 3.14f;
+				theta = (j + 1) / 18f * 3.14f; 
 				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
 				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
 				z = r * Mathf.Cos (theta);
-				end.setPoint (x, y, z);
-				actin.Add (start);
-				actin.Add (end);
-
-				phi = i-1  / 18f * 3.14f;
-				theta = j-1  / 18f * 3.14f;
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 + 10) / 180f * 3.14f;
+				theta = (j + 1) / 18f * 3.14f;
 				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
 				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
 				z = r * Mathf.Cos (theta);
-				end.setPoint (x, y, z);
-				actin.Add (start);
-				actin.Add (end);
-
-				phi = i+1  / 18f * 3.14f;
-				theta = j  / 18f * 3.14f;
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 - 10) / 180f * 3.14f;
+				theta = (j + 1) / 18f * 3.14f;
 				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
 				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
 				z = r * Mathf.Cos (theta);
-				end.setPoint (x, y, z);
-				actin.Add (start);
-				actin.Add (end);;
-
-				phi = i+1  / 18f * 3.14f;
-				theta = j+1  / 18f * 3.14f;
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				
+				
+				
+				phi = i / 18f * 3.14f;
+				theta = (j - 1) / 18f * 3.14f; 
 				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
 				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
 				z = r * Mathf.Cos (theta);
-				end.setPoint (x, y, z);
-				actin.Add (start);
-				actin.Add (end);
-
-				phi = i+1  / 18f * 3.14f;
-				theta = j-1  / 18f * 3.14f;
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 + 10) / 180f * 3.14f;
+				theta = (j - 1) / 18f * 3.14f;
 				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
 				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
 				z = r * Mathf.Cos (theta);
-				end.setPoint (x, y, z);
-				actin.Add (start);
-				actin.Add (end);*/
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 - 10) / 180f * 3.14f;
+				theta = (j - 1) / 18f * 3.14f;
+				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				z = r * Mathf.Cos (theta);
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube); 
 
+
+			}
+		}
+
+
+
+
+		for (var j=0; j<19; j++) {
+			//var j = 1 ;
+			for (var i = 0; i < 36; i++) {
+				r = layer1;
+				phi = i / 18f * 3.14f;
+				theta = j / 18f * 3.14f;
+				float x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				float y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				float z = r * Mathf.Cos (theta);
+				var start = new Vector3 (x, y, z);
+				
+				r = layer2;
+				phi = i / 18f * 3.14f;
+				theta = j / 18f * 3.14f; 
+				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				z = r * Mathf.Cos (theta);
+				var end = new Vector3 (x, y, z);
+				var microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 + 10) / 180f * 3.14f;
+				theta = j / 18f * 3.14f;
+				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				z = r * Mathf.Cos (theta);
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 - 10) / 180f * 3.14f;
+				theta = j / 18f * 3.14f;
+				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				z = r * Mathf.Cos (theta);
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				
+				
+				
+				phi = i / 18f * 3.14f;
+				theta = (j + 1) / 18f * 3.14f; 
+				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				z = r * Mathf.Cos (theta);
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 + 10) / 180f * 3.14f;
+				theta = (j + 1) / 18f * 3.14f;
+				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				z = r * Mathf.Cos (theta);
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 - 10) / 180f * 3.14f;
+				theta = (j + 1) / 18f * 3.14f;
+				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				z = r * Mathf.Cos (theta);
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				
+				
+				
+				phi = i / 18f * 3.14f;
+				theta = (j - 1) / 18f * 3.14f; 
+				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				z = r * Mathf.Cos (theta);
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 + 10) / 180f * 3.14f;
+				theta = (j - 1) / 18f * 3.14f;
+				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				z = r * Mathf.Cos (theta);
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube);
+				
+				phi = (i * 10 - 10) / 180f * 3.14f;
+				theta = (j - 1) / 18f * 3.14f;
+				x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+				y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+				z = r * Mathf.Cos (theta);
+				end = new Vector3 (x, y, z);
+				microTube = MicroTube.Instantiate (start, end);
+				microTube.ApplyColor (color);
+				microTube.ApplyWidth (width);
+				microTube.transform.SetParent (transform, false);
+				actin.Add (microTube); 
+
+				
+			}
 		}
 	}
 
